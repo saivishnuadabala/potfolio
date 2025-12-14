f4d0e3005204a715e26224ad990a45ebc90e6b72
Sai Vishnu <saivishnu@cracku.in>
2025-12-15 00:52:50 +0530

    Initial commit1

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true,
+	  },
 	images: {
 		domains: ['img.shields.io', 'storage.googleapis.com','cdn.jsdelivr.net'],
 		dangerouslyAllowSVG: true,
